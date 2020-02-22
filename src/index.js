6f13b9498e2dae5cb9fed51479b9f7a484f71760
Spawn <alkspawn@gmail.com>
2020-02-22 21:31:23 +0300

    feat: add logic

--- src/index.js
+++ src/index.js
@@ -1,6 +1,15 @@
-
-// You should implement your task here.
-
 module.exports = function towelSort (matrix) {
-  return [];
+  if (!Array.isArray(matrix) ) {
+    return [];
+}
+  let array = [];
+  matrix.forEach((element,count) => {
+    if(count%2){
+        let temp = element;
+     array=array.concat(temp.reverse())
+    } else{
+ array=array.concat(element)
+    }
+});
+return array;
 }
